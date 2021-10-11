dfe1b15ee0b86b35fc546fbfe366c25621c3a885
Al azery <81170076+AlAzery@users.noreply.github.com>
2021-10-11 16:38:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Insert angka ke textview
+function insert(num){
+	document.form.textarea.value = document.form.textarea.value + num;
+}
+
+// Fungsi Hitung
+function equal(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = eval(hasil);
+}
+
+// Clean
+function clean(){
+	document.form.textarea.value = "";
+}
+
+// Delete
+function back(){
+	var hasil = document.form.textarea.value;
+	document.form.textarea.value = hasil.substring(0,hasil.length-1);
+}
